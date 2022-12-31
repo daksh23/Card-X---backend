8b5e46eb186785f1b29c3f7258a27340714c35fe
Daksh Patel <dakshpatel664@gmail.com>
2022-12-31 17:14:06 -0500

    Project Created - CardX Apis

---
+++ src/main/java/com/cardx/Cardx/CardxApplication.java
@@ -0,0 +1,13 @@
+package com.cardx.Cardx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CardxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CardxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cardx/Cardx/CardxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cardx.Cardx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CardxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
