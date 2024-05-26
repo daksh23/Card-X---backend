85d7b4bce204828c04d6954c699394af10280fc1
Daksh Patel <dakshpatel664@gmail.com>
2024-05-26 16:40:35 -0400

    cors config added to allow localhost calls from ui

--- src/main/java/com/cardx/Cardx/Controllers/CardxControllers.java
+++ src/main/java/com/cardx/Cardx/Controllers/CardxControllers.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@CrossOrigin(origins = "http://localhost:4200") // Specify the allowed origin
 @RestController
 @RequestMapping("cardx/rest/v1")
 public class CardxControllers {
